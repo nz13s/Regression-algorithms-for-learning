41df367315029704f8b927d82067a477d1b84c56
nz13s <nikita.bogachev.2018@live.rhul.ac.uk>
2020-11-01 21:28:25 +0000

    - Edited KNN function to remove unnecessary "for loop" for label reassignment, could be done in the same for loop. Faster code! - Included ionosphere.txt and the test for the dataset. - Committing to master for now, next algorithm will get its own branch.

--- main/KNNClassifier.py
+++ main/KNNClassifier.py
@@ -18,23 +18,23 @@ def KNNClassifier(X_train, y_train, testTuple, k):
     The idea is to run this algorithm against every point in the X_train set.
     :param X_train: a matrix of X values to be used as a training set.
     :param y_train: a matrix of labels for X_train values.
-    :param testTuple: a tuple from X_test array to be tested, e.g. X_test[0]
+    :param testTuple: a tuple from X_test array to be tested, e.g. X_test[0] for Iris.
     :param k: number of nearest neighbors.
-    :return: classification label of a point.
+    :return: classification label of a point (or multiple points).
     """
 
     distance = []  # Array to store distances to nearest neighbors
 
-    # Append distances, but set label as temporary 0
+    # Append distances, point and its label
     for data in range(len(X_train)):
         E_distance = np.linalg.norm(X_train[data] - testTuple)
-        distance.append([X_train[data], E_distance, 0])
-
-    # Adjust the labels according to labels in y_train
-    for i in range(len(y_train)):
-        distance[i][2] = y_train[i]
+        distance.append([X_train[data], E_distance, y_train[data]])
 
     # Sort distance
+    """
+    This sorts distances by actual distances, thus we use index of 1 corresponding to 
+    distance[x][1] which is distance.
+    """
     distance.sort(key=operator.itemgetter(1))  # REPLACE THIS LATER
 
     # Create the neighbors array
--- test/KNNTest.py
+++ test/KNNTest.py
@@ -5,12 +5,15 @@ import numpy as np
 from main.KNNClassifier import KNNClassifier
 
 
-class KNNTest:
+class TestIris:
+    """Iris KNN implementation:"""
     iris = load_iris()
 
+    # 1512 is my birthday in the DDMM format for a pseudo-random number generator as said in the assignment.
     X_train, X_test, y_train, y_test = train_test_split(iris['data'],
                                                         iris['target'],
                                                         random_state=1512)
+
     k = 3
     y_pred = []
     for entry in X_test:
@@ -19,4 +22,45 @@ class KNNTest:
         y_pred.append(current_label)
 
     accuracy = np.mean(y_pred == y_test)
+    print("Error rate (rounded) is {}%".format(round((1 - accuracy) * 100)))
+
+
+class TestIonSphere:
+    """Ionosphere implementation"""
+    X_ionosphere = np.genfromtxt("ionosphere.txt",
+                                 delimiter=",",
+                                 usecols=np.arange(34))
+    y_ionosphere = np.genfromtxt("ionosphere.txt",
+                                 delimiter=",",
+                                 usecols=np.arange(34),
+                                 dtype='int')
+
+    # 1512 is my birthday in the DDMM format for a pseudo-random number generator as said in the assignment.
+    X_train, X_test, y_train, y_test = train_test_split(X_ionosphere,
+                                                        y_ionosphere,
+                                                        random_state=1512)
+
+    """
+    Ionosphere is built different from Iris dataset.
+    For Iris, X_train[0] called a single "tuple" and y_train[0] called its label.
+    For IS, an array of tuples is called from X_train[0] so we have to include
+    an index and a while loop.
+    """
+
+    print("--------------------")
+
+    # 3 nearest neighbors
+    k = 3
+    y_pred_total = []
+    for entry in range(len(X_test)):  # take the first array
+        index = 0
+        y_pred_this_array = []
+        while index < len(X_test[entry]):
+            current_label = KNNClassifier(X_train[entry], y_train[entry], X_test[entry][index], k)
+            print("The predicted label is {}".format(current_label))
+            y_pred_this_array.append(current_label)
+            index += 1
+        y_pred_total.append(y_pred_this_array)
+
+    accuracy = np.mean(y_pred_total == y_test)
     print("Error rate (rounded) is {}%".format(round((1 - accuracy) * 100)))
