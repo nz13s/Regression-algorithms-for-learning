ad1a2ad24903216918664ff8c461b4fe09868d62
nz13s <nikita.bogachev.2018@live.rhul.ac.uk>
2020-11-02 11:51:20 +0000

    Fixed the test for ionosphere to initialise y_ionosphere correctly. Replaced the for loop accordingly.

--- test/KNNTest.py
+++ test/KNNTest.py
@@ -32,7 +32,7 @@ class TestIonSphere:
                                  usecols=np.arange(34))
     y_ionosphere = np.genfromtxt("ionosphere.txt",
                                  delimiter=",",
-                                 usecols=np.arange(34),
+                                 usecols=34,
                                  dtype='int')
 
     # 1512 is my birthday in the DDMM format for a pseudo-random number generator as said in the assignment.
@@ -51,16 +51,11 @@ class TestIonSphere:
 
     # 3 nearest neighbors
     k = 3
-    y_pred_total = []
-    for entry in range(len(X_test)):  # take the first array
-        index = 0
-        y_pred_this_array = []
-        while index < len(X_test[entry]):
-            current_label = KNNClassifier(X_train[entry], y_train[entry], X_test[entry][index], k)
-            print("The predicted label is {}".format(current_label))
-            y_pred_this_array.append(current_label)
-            index += 1
-        y_pred_total.append(y_pred_this_array)
+    y_pred = []
+    for entry in X_test:
+        current_label = KNNClassifier(X_train, y_train, entry, k)
+        print("The predicted label is {}".format(current_label))
+        y_pred.append(current_label)
 
-    accuracy = np.mean(y_pred_total == y_test)
+    accuracy = np.mean(y_pred == y_test)
     print("Error rate (rounded) is {}%".format(round((1 - accuracy) * 100)))
