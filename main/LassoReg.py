97bbee48f29e582128ab38b84fc502c12edcc699
nz13s <nikita.bogachev.2018@live.rhul.ac.uk>
2021-02-19 23:47:47 +0300

    Lasso Regression class and unit test OOP redesign fro LassoReg.py and a unit test class. Fixes to LassoReg and LassoRegTest.py.

--- main/LassoReg.py
+++ main/LassoReg.py
@@ -1,121 +1,117 @@
 import numpy as np
-from sklearn.model_selection import train_test_split
 
 
-def lasso_reg(data, target, alpha):
-    """
-    Build a model for LASSO regression given a dataset of X and Y.
+class LassoReg:
+    coeffs = []
+    y_pred = []
 
-    In ridge regression, we added a value of alpha α
-    w = (X'X + αI)^-1 X' Y
-    where I is the identity matrix of X'X and b (the intercept b0) is the first element of w[].
+    def __init__(self, X_train, y_train, alpha):
+        self.X_train = X_train
+        self.y_train = y_train
+        self.alpha = alpha
 
-    From Chetan Patil's comment on https://stats.stackexchange.com/questions/176599/, we can see that:
+    def fit(self):
+        """
+        Build a model for LASSO regression given a dataset of X and Y.
 
-    In ridge regression, for one w, w = xy / (x^2 + α). The denominator only becomes zero when α --> ∞.
-    In LASSO regression, for one w, w = (2xy - α) / (2x^2). The numerator will become zero since we subtract α,
-    making large values of w equal to 0.
+        In ridge regression, we added a value of alpha α
+        w = (X'X + αI)^-1 X' Y
+        where I is the identity matrix of X'X and b (the intercept b0) is the first element of w[].
 
-    From "Lasso Regression" by Wessel van Wieringen
-    (http://www.few.vu.nl/~wvanwie/Courses/HighdimensionalDataAnalysis/WNvanWieringen_HDDA_Lecture56_LassoRegression_20182019.pdf)
-    I have found the following formula: X'X w = X'Y - 0.5 α ż, where ż(j) = sign{[w(α)]j}.
-    Therefore, I believe the Lasso coefficient formula can be written as:
+        From Chetan Patil's comment on https://stats.stackexchange.com/questions/176599/, we can see that:
 
-    w(a) = (X'X)^-1 (X'Y - 0.5αz)
-    When w(a) < 0, z = -1, making the formula to be w = (X'X)^-1 (X'Y + 0.5α), bringing it closer to zero.
-    When w(a) > 0, z = 1, making the formula to be w = (X'X)^-1 (X'Y - 0.5α), bringing it closer to zero.
-    When w(a) = 0, z ∈ [-1,1] (which does not help to make this any more understandable)
+        In ridge regression, for one w, w = xy / (x^2 + α). The denominator only becomes zero when α --> ∞.
+        In LASSO regression, for one w, w = (2xy - α) / (2x^2). The numerator will become zero since we subtract α,
+        making large values of w equal to 0.
 
-    This is also evident in Patil's formula which will look similar if the numerator and denominator would be
-    divided by 2. However, it is difficult to understand or code as it requires from us to know w and z simultaneously,
-    which is not possible.
+        From "Lasso Regression" by Wessel van Wieringen
+        (http://www.few.vu.nl/~wvanwie/Courses/HighdimensionalDataAnalysis/WNvanWieringen_HDDA_Lecture56_LassoRegression_20182019.pdf)
+        I have found the following formula: X'X w = X'Y - 0.5 α ż, where ż(j) = sign{[w(α)]j}.
+        Therefore, I believe the Lasso coefficient formula can be written as:
 
-    A research from HSE (https://www.hse.ru/data/2018/03/15/1164355911/4.%20Ridge%20Regression%20and%20Lasso%20v1.pdf)
-    presents an interesting and simple formula that builds on the Least Squares method:
+        w(a) = (X'X)^-1 (X'Y - 0.5αz)
+        When w(a) < 0, z = -1, making the formula to be w = (X'X)^-1 (X'Y + 0.5α), bringing it closer to zero.
+        When w(a) > 0, z = 1, making the formula to be w = (X'X)^-1 (X'Y - 0.5α), bringing it closer to zero.
+        When w(a) = 0, z ∈ [-1,1] (which does not help to make this any more understandable)
 
-    When w(LS) > 0, w(Lasso) = w(LS) - 0.5α.
-    When w(LS) < 0, w(Lasso) = w(LS) + 0.5α.
-    This formula combined will look like this:
+        This is also evident in Patil's formula which will look similar if the numerator and denominator would be
+        divided by 2. However, it is difficult to understand or code as it requires from us to know w and z simultaneously,
+        which is not possible.
 
-    w(Lasso) = sign(w(LS)) (|w(LS)| - 0.5α)+
+        A research from HSE (https://www.hse.ru/data/2018/03/15/1164355911/4.%20Ridge%20Regression%20and%20Lasso%20v1.pdf)
+        presents an interesting and simple formula that builds on the Least Squares method:
 
-    The subscript (x)+ means that this bracket will be the maximum result of max(x, 0).
+        When w(LS) > 0, w(Lasso) = w(LS) - 0.5α.
+        When w(LS) < 0, w(Lasso) = w(LS) + 0.5α.
+        This formula combined will look like this:
 
-    If this does not work, I don't know what will.
+        w(Lasso) = sign(w(LS)) (|w(LS)| - 0.5α)+
 
-    Like in Ridge, b (the intercept b0) is the first element of w[].
-    Otherwise, it operates in a similar way to other linear regression problems with:
-    y = b0 * 1 + b1x1+...+ bkxk for both methods (simple and multiple).
-    :param data: a matrix of X values.
-    :param target: a matrix of labels for the data values.
-    :return R2: accuracy score
-    """
+        The subscript (x)+ means that this bracket will be the maximum result of max(x, 0).
 
-    # Since we are using a uniform w formula, we don't need two methods for finding w and b.
-    # Still, we need to reshape data if it only has one feature.
-    if len(data.shape) == 1:
-        data = data.reshape(-1, 1)
+        If this does not work, I don't know what will.
 
-    # Split the data.
-    X_train, X_test, y_train, y_test = train_test_split(data,
-                                                        target,
-                                                        random_state=1512)
+        Like in Ridge, b (the intercept b0) is the first element of w[].
+        Otherwise, it operates in a similar way to other linear regression problems with:
+        y = b0 * 1 + b1x1+...+ bkxk for both methods (simple and multiple).
+        """
 
-    # Concatenate ones to X.
-    ones = np.ones(shape=X_train.shape[0]).reshape(-1, 1)
-    X_train = np.concatenate((ones, X_train), 1)
+        # Concatenate ones to X.
+        ones = np.ones(shape=self.X_train.shape[0]).reshape(-1, 1)
+        self.X_train = np.concatenate((ones, self.X_train), 1)
 
-    '''
-    This is needed for the formula for the matrical product to work, as:
+        '''
+        This is needed for the formula for the matrical product to work, as:
+    
+        y = b0 * >1< + b1x1 + b2x2 + ... + bkxk
+    
+        Therefore, we need a column of ones (highlighted as >1<) in our X_train to satisfy the intercept b0
+        at w[0].
+        '''
 
-    y = b0 * >1< + b1x1 + b2x2 + ... + bkxk
+        #  Calculate coefficients from the formula in the introduction passage
+        # w(LS) = (X'X)^-1 X'Y
+        w_least_squares = np.linalg.solve(np.transpose(self.X_train) @ self.X_train,
+                                          np.transpose(self.X_train) @ self.y_train)
 
-    Therefore, we need a column of ones (highlighted as >1<) in our X_train to satisfy the intercept b0
-    at w[0].
-    '''
+        # If matrix is orthogonal, then: w(Lasso) = sign(w(LS)) (|w(LS)| - 0.5α)+
+        if np.transpose(self.X_train).dot(self.X_train) == np.identity(len(self.X_train)):
+            self.coeffs = list(map(lambda w: np.sign(w) * max(abs(w) - (self.alpha / 2), 0), w_least_squares))
 
-    #  Calculate coefficients from the formula in the introduction passage
-    # w(LS) = (X'X)^-1 X'Y
-    w_least_squares = np.linalg.solve(np.transpose(X_train) @ X_train,
-                                      np.transpose(X_train) @ y_train)
+        # (X'X) * w(Lasso) = (X'Y - 0.5α * sign(w(LS))
+        else:
+            # we make a list of sign values of LS coefficients
+            sign_list = []
+            for w in w_least_squares:
+                i = np.sign(w)  # -1, 1 or 0 if w=0
+                sign_list.append(int(i))
+            # solve using formula?
+            signs = np.copy(sign_list)
+            self.coeffs = np.linalg.solve(self.X_train.T @ self.X_train,
+                                          self.X_train.T @ self.y_train - (self.alpha / 2) * signs)
 
-    # If matrix is orthogonal, then: w(Lasso) = sign(w(LS)) (|w(LS)| - 0.5α)+
-    if np.transpose(X_train).dot(X_train) == np.identity(len(X_train)):
-        coeffs = list(map(lambda w: np.sign(w) * max(abs(w) - (alpha / 2), 0), w_least_squares))
+        for i, coef in enumerate(self.coeffs):
+            if abs(coef) <= self.alpha / 2:
+                self.coeffs[i] = 0
 
-    # (X'X) * w(Lasso) = (X'Y - 0.5α * sign(w(LS))
-    else:
-        # we make a list of sign values of LS coefficients
-        sign_list = []
-        for w in w_least_squares:
-            i = np.sign(w)  # -1, 1 or 0 if w=0
-            sign_list.append(int(i))
-        # solve using formula?
-        signs = np.copy(sign_list)
-        coeffs = np.linalg.solve(X_train.T @ X_train,
-                                 X_train.T @ y_train - (alpha/2) * signs)
+    def predict(self, X_test):
+        """
+        From the formula, we can find
+        y = b0 + b1x1 + b2x2 + .... bkxk
+        """
+        intercept = self.coeffs[0]  # first element is the intercept b0
+        b_vals = self.coeffs[1:]  # the rest of coefficients starting from b[1]
+        pred = []  # array for predictions
 
-    for i, coef in enumerate(coeffs):
-        if abs(coef) <= alpha/2:
-            coeffs[i] = 0
+        for entry in X_test:
+            y_current = intercept  # start as y = b0 + ...
+            for xi, bi in zip(entry, b_vals):
+                y_current += bi * xi  # keep adding
+            pred.append(y_current)
+        self.y_pred = np.copy(pred)
 
-    '''
-    From the formula, we can find
-    y = b0 + b1x1 + b2x2 + .... bkxk
-    '''
-    intercept = coeffs[0]  # first element is the intercept b0
-    b_vals = coeffs[1:]  # the rest of coefficients starting from b[1]
-    pred = []  # array for predictions
-    print("w = {}, b = {}".format(b_vals, intercept))
-
-    for entry in X_test:
-        y_current = intercept  # start as y = b0 + ...
-        for xi, bi in zip(entry, b_vals):
-            y_current += bi * xi  # keep adding
-        pred.append(y_current)
-
-    y_pred = np.copy(pred)
-    u = ((y_test - y_pred) ** 2).sum()
-    v = ((y_test - y_test.mean()) ** 2).sum()
-    R2 = 1 - (u / v)
-    return R2
+    def score(self, y_test):
+        u = ((y_test - self.y_pred) ** 2).sum()
+        v = ((y_test - y_test.mean()) ** 2).sum()
+        R2 = 1 - (u / v)
+        return R2
--- test/LassoTest.py
+++ test/LassoTest.py
@@ -1,67 +1,50 @@
+import unittest
 import numpy as np
 from sklearn.datasets import load_boston
-from sklearn.linear_model import Lasso
 from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
 
-from main.LassoReg import lasso_reg
+from main.LassoReg import LassoReg
 
 
-class LassoTest:
-    alpha = 0.5
-
-    # Testing simple LR
-    print("Simple Lasso Regression")
+class LassoRegTest(unittest.TestCase):
+    alpha = 0.9
     x = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8])
     y = np.array([-1, 0.2, 0.9, 2.1, 3.4, 4.2, 5.6, 6.5, 7.3])
-    R2 = lasso_reg(x, y, alpha=alpha)
-    print("My R2:", R2)
-
-    # For simple LR, we need to reshape X here in order for TTT to work.
-    # We don't need to do it when setting up x as my lin_reg() reshapes it inside the function.
     X_train, X_test, y_train, y_test = train_test_split(x.reshape(-1, 1),
                                                         y,
                                                         random_state=1512)
-    lr = Lasso(alpha=alpha).fit(X_train, y_train)
-    # print(lr.coef_, lr.intercept_)
-    print("Scikit R2:", lr.score(X_test, y_test))
-
-    print("---")
-
-    print("Multiple Lasso regression")
     boston = load_boston()
-    X_train, X_test, y_train, y_test = train_test_split(boston.data, boston.target, random_state=1512)
-    lasso = Lasso(alpha=alpha).fit(X_train, y_train)
-    R2 = lasso_reg(boston.data, boston.target, alpha=alpha)
-    print("My R2:", R2)
-    # print(lasso.coef_, lasso.intercept_)
-    print("Scikit R2:", lasso.score(X_test, y_test))
+    boston_x_train, boston_x_test, boston_y_train, boston_y_test = train_test_split(boston.data,
+                                                                                    boston.target,
+                                                                                    random_state=1512)
 
-    print("---")
-    alphas = [0 for x in range(100)]
-    for i in range(len(alphas)):
-        alphas[i] = alphas[i - 1] + 0.01
+    lasso_simple = LassoReg(X_train, y_train, alpha)
+    lasso_simple.fit()
 
-    my_r2 = []
-    scikit_r2 = []
-    for alpha in alphas:
-        lasso = Lasso(alpha=alpha, max_iter=100000).fit(X_train, y_train)
-        my_R2 = lasso_reg(boston.data, boston.target, alpha=alpha)
-        sci_R2 = lasso.score(X_test, y_test)
-        scikit_r2.append(sci_R2)
-        my_r2.append(my_R2)
-        plt.scatter(alpha, sci_R2,
-                    # marker=icon,
-                    label="Lasso alpha={}".format(alpha),
-                    linewidths=2)
-        plt.scatter(alpha, my_R2,
-                    # marker=icon,
-                    label="Lasso alpha={}".format(alpha),
-                    linewidths=2)
+    lasso_boston = LassoReg(boston_x_train, boston_y_train, alpha)
+    lasso_boston.fit()
 
-    plt.plot(alphas, my_r2, label="My accuracy")
-    plt.plot(alphas, scikit_r2, label="Goal accuracy")
-    # plt.legend()
-    plt.ylabel("R2 score")
-    plt.xlabel("Alpha value")
-    plt.show()
+    def test_fit(self):
+        self.assertTrue(len(self.lasso_simple.coeffs))
+        self.assertTrue(len(self.lasso_boston.coeffs))
+
+    def test_predict(self):
+        self.lasso_simple.predict(self.X_test)
+        self.assertTrue(len(self.lasso_simple.y_pred))
+
+        self.lasso_boston.predict(self.boston_x_test)
+        self.assertTrue(len(self.lasso_boston.y_pred))
+
+    def test_score(self):
+        pred_copy_simple = np.copy(self.lasso_simple.y_pred)
+        pred_copy_boston = np.copy(self.lasso_boston.y_pred)
+
+        self.assertTrue(len(self.y_test))
+        self.assertTrue(len(self.boston_y_test))
+
+        self.assertEqual(pred_copy_simple.shape, self.y_test.shape)
+        self.assertEqual(pred_copy_boston.shape, self.boston_y_test.shape)
+
+
+if __name__ == '__main__':
+    unittest.main()
