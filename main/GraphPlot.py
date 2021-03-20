6084b0f971027ad2e6d8536307c843334b90988f
nz13s <nikita.bogachev.2018@live.rhul.ac.uk>
2021-03-20 19:13:34 +0300

    Colored GraphPlot Now every "column" is colored on a plot. Way too long running time, as it is a double for loop that iterates through the whole matrix item by item and plots each X value in a row againts y from the same row, iterating through an array of colors of the size of said row. Looks colourful but may be unnecessary for the project itself as I need a best fit line through a scatter plot of original data, making the scatter coloured may not be needed?

--- main/GraphPlot.py
+++ main/GraphPlot.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
+from random import randint
 import numpy as np
 from sklearn.datasets import load_iris, load_boston
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsRegressor
 from sklearn.preprocessing import StandardScaler
 
 from main.KNN import KNN
@@ -10,36 +10,23 @@ from main.RegressionModel import RegressionModel
 
 
 class GraphPlot:
-    def __init__(self, model_input):
-        self.model = model_input
+    def __init__(self):
+        pass
 
-    def plot(self, mode, X_test, **kwargs):
+    def plot(self, X_train, y_train):
         """
         Plot the model results with mode selection for pure results, accuracy comparisons or overlap
-        :param X_test: testing data
-        :param mode: "predict" or "compare"
-        :param kwargs: optional argument to include y_test if known to compare and see overlap
+        :param X_train: training set of X values
+        :param y_train: training set of y labels
         """
-        X = []
-        Y_test = []
-        Y_pred = []
-        y_test = kwargs.get("y_test")
-        if mode == "predict":
-            for x_row, y in zip(X_test, self.model.y_pred):
-                for item in x_row:
-                    X.append(item)
-                    Y_pred.append(y)
-            plt.scatter(X, Y_pred, label="Prediction", c="green")
+        # Make array of colors
+        colors = []
+        for i in range(X_train.shape[1]):
+            colors.append('#%06X' % randint(0, 0xFFFFFF))
 
-        if mode == "compare":
-            for x_row, y1, y2 in zip(X_test, y_test, self.model.y_pred):
-                for item in x_row:
-                    X.append(item)
-                    Y_test.append(y1)
-                    Y_pred.append(y2)
-            plt.scatter(X, Y_test, label="Original data", c="blue")
-            plt.scatter(X, Y_pred, label="Prediction", c="green")
-        plt.legend()
+        for x_row, y in zip(X_train, y_train):
+            for i in range(len(x_row)):
+                plt.scatter(x_row[i], y, color=colors[i])
         plt.ylabel("Y")
         plt.xlabel("Data")
         plt.show()
@@ -48,7 +35,22 @@ class GraphPlot:
     # Currently broken, trying to re-create the function from Nov 10th but with the new KNN structure and it wworks once
     # and then gives a constant score of 1.0 for k = 2 to k = len(X_train).
 
-    def regression_accuracy_plot(self, X_train, y_train, X_test, y_test):
+    def ridge_accuracy(self, X_train, y_train, X_test, y_test):
+        alpha_values = np.append(np.arange(0.0, 1.0, 0.01), 1.0)
+        scores = []
+        for a in alpha_values:
+            reg_model = RegressionModel(X_train, y_train)
+            reg_model.ridge_fit(alpha=a)
+            reg_model.predict(X_test)
+            score = reg_model.score(y_test)
+            scores.append(score)
+            plt.scatter(a, score, label="Alpha value {}".format(a))
+        plt.plot(alpha_values, scores)
+        plt.xlabel("Alpha values")
+        plt.ylabel("R2 scores")
+        plt.show()
+
+    def lasso_accuracy(self, X_train, y_train, X_test, y_test):
         alpha_values = np.append(np.arange(0.0, 1.0, 0.01), 1.0)
         scores = []
         for a in alpha_values:
@@ -65,18 +67,17 @@ class GraphPlot:
 
 
 class Main:
-    # iris = load_iris()
-    # X_train, X_test, y_train, y_test = train_test_split(iris.data,
-    #                                                     iris.target,
-    #                                                     random_state=1512)
-    # myKNN = KNN(X_train, y_train, k=3, alg_type='r')
-    # myKNN.predict(X_test)
-    # myplot = GraphPlot(myKNN)
-    # myplot.plot(mode="predict", X_test=X_test, y_test=y_test)
-    # score = myKNN.score(y_test)
-    # print(score)
-    # print(myKNN.y_pred)
-    #
+    iris = load_iris()
+    X_train, X_test, y_train, y_test = train_test_split(iris.data,
+                                                        iris.target,
+                                                        random_state=1512)
+    myKNN = KNN(X_train, y_train, k=3, alg_type='r')
+    myKNN.predict(X_test)
+    myplot = GraphPlot()
+    myplot.plot(X_train, y_train)
+    score = myKNN.score(y_test)
+    print(score)
+
     # myplot.knn_accuracy_plot(X_train, y_train, X_test, y_test)
 
     boston = load_boston()
@@ -92,7 +93,7 @@ class Main:
     reg_model.lasso_fit(alpha=0.8)
     reg_model.predict(X_test=boston_X_test_scaled)
     print(reg_model.score(boston_y_test))
-    regPlot = GraphPlot(reg_model)
-    regPlot.plot(mode="compare", X_test=boston_X_test_scaled, y_test=boston_y_test)
+    regPlot = GraphPlot()
+    regPlot.plot(X_train=boston_X_train_scaled, y_train=boston_y_train)
 
-    regPlot.regression_accuracy_plot(boston_X_train_scaled, boston_y_train, boston_X_test_scaled, boston_y_test)
+    regPlot.lasso_accuracy(boston_X_train_scaled, boston_y_train, boston_X_test_scaled, boston_y_test)
